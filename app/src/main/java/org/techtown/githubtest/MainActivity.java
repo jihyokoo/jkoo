a1396065f3ae1be7948f4453e14bdf76bc0d2507
jihyokoo <stephane0549@naver.com>
2020-03-25 22:45:42 +0900

    아무말

---
+++ app/src/main/java/org/techtown/githubtest/MainActivity.java
@@ -0,0 +1,16 @@
+package org.techtown.githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //GitHub 연동 테스트
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
